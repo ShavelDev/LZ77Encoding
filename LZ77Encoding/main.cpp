8af35a7690a0db655db6c2bd29e45fce7159b12a
ShavelDev <nor.szaw@gmail.com>
2024-10-18 13:35:21 +0200

    Initial Commit

---
+++ LZ77Encoding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LZ77Encoding
+//
+//  Created by Norbert Szawloga on 18/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
